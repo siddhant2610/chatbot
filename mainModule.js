c2b31b8e257c5e4264a2182c0c112a26adeaf7b7
Sidhant Upadhyay <siddhantupadhyay26@gmail.com>
2024-06-10 15:32:03 +0530

    Update mainModule.js

--- mainModule.js
+++ mainModule.js
@@ -2,7 +2,7 @@
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyAO6iAD-wRFXYMDrQxBBkQlKnOrIgLDbXo"; // Paste your API key here
+const API_KEY = "AIzaSyDglamIbegXkxNX1Bvx2VpjhWpRMoxMN0w"; // Paste your API key here
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 // Export the 'model' variable for use in other modules
